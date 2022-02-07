ec4c7ef347e051269264e7142b5d9dbabedfba47
Wojciech Grzybek <229258@edu.p.lodz.pl>
2022-02-07 17:49:23 +0100

    1.4 accessory 4 Usunięcie nieużywanych setterów.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -42,15 +42,7 @@ public class IntLinkedList {
 		return last;
 	}
 
-	void setLast(Node last) {
-		this.last = last;
-	}
-
 	int getI() {
 		return i;
-	}
-
-	void setI(int i) {
-		this.i = i;
 	}
 }

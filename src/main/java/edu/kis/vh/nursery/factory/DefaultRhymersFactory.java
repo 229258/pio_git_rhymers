079089b02091c1dfb7d280f21e3f331903ef0105
Wojciech Grzybek <229258@edu.p.lodz.pl>
2022-02-12 20:25:50 +0100

    1.5.6 Wygenerowanie i uzupełnienie dokumentacji dla klasy DefaultRhymersFactory.

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -5,26 +5,57 @@ import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
+/**
+ * The factory class implements RhymersFactory and creates
+ * DefaultCountingOutRhymer objects of different types
+ * 
+ * @author Wojciech Grzybek
+ * @see RhymersFactory
+ * @see DefaultCountingOutRhymer
+ */
 public class DefaultRhymersFactory implements RhymersFactory {
 
+	/**
+	 * Creates a DefaultCountingOutRhymer object
+	 *
+	 * @return DefaultCountingOutRhymer object
+	 * @see DefaultCountingOutRhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getStandardRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
+	/**
+	 * Creates a DefaultCountingOutRhymer object
+	 *
+	 * @return DefaultCountingOutRhymer object
+	 * @see DefaultCountingOutRhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFalseRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
+	/**
+	 * Creates a FIFORhymer object
+	 *
+	 * @return FIFORhymer object
+	 * @see FIFORhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFIFORhymer() {
 		return new FIFORhymer();
 	}
 
+	/**
+	 * Creates a HanoiRhymer object
+	 *
+	 * @return HanoiRhymer object
+	 * @see HanoiRhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getHanoiRhymer() {
 		return new HanoiRhymer();
 	}
-
 }
